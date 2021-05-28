79d3b647eca575a3bd8db3032ba9f0a07a1950b8
Dstrate <76860875+Dstrate@users.noreply.github.com>
2021-05-28 22:24:41 +0700

    Initial commit

---
+++ src/pages/contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-8">
+    <figure class="bg-gray-100 rounded-xl p-8 shadow-md">
+      <img
+        class="w-32 h-32 rounded-full mx-auto"
+        src="../assets/profile2.jpg"
+      />
+      <div class="pt-6 text-center space-y-4">
+        <figcaption class="font-medium">
+          <div class="text-gray-500 text-center">
+            <li>tel : {{ tel }}</li>
+            <li>email : {{ email }}</li>
+            <li>facebook : {{ facebook }}</li>
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: "peach.14975@gmail.com",
+    facebook: "Peach Gsw",
+    tel: "0800436798",
+  }),
+};
+</script>
---
+++ src/pages/info.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="p-8">
+    <figure class="xl:flex bg-gray-100 rounded-xl p-8 shadow-md">
+      <img
+        class="w-32 h-32 rounded-full mx-auto"
+        src="../assets/profile1.jpg"
+      />
+      <div class="pt-6 text-center space-y-4">
+        <blockquote>
+          <p class="text-lg font-semibold">
+            {{ name }} {{ surname }} ({{ age }})
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-cyan-600">{{ university }}</div>
+          <div class="break-words text-gray-500">
+            <span v-for="skill in skills" :key="skill">
+              {{ skill }}
+              <span v-if="skill != skills[skills.length - 1]">, </span>
+            </span>
+          </div>
+          <div class="pt-6">
+            <router-link :to="{ name: 'contact' }">
+              <a class="text-gray-500 hover:text-yellow-500">
+                <strong>contact ></strong>
+              </a>
+            </router-link>
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: "Methichai",
+    surname: "Preedanorawut",
+    age: 21,
+    university: "Thai-Nichi Institute of Technology",
+    skills: ["Python", "JS", "HTML", "CSS", "SQL"],
+  }),
+};
+</script>
